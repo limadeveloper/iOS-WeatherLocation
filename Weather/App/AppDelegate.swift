e5554530ef407ae49ef754063c96509b06207162
John Lima <john.limadeveloper@gmail.com>
2018-05-26 13:22:12 -0300

    Initial setup
    Set Swift as main language
    Set minimum version
    Set folders
    Set cocoapods as a dependency manager
    Add .gitignore

---
+++ Weather/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Weather
+//
+//  Created by John Lima on 26/05/18.
+//  Copyright © 2018 limadeveloper. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
